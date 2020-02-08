b969d0b109aaa3046249567b6fa6e48471db9cd2
JunZhen-Wang <juwa4452@colorado.edu>
2020-02-07 17:27:11 -0700

    summit email_results.txt file

---
+++ lab3_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Authors : JunZhen Wang
+# Date: 1/1/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+echo "Enter a file name: "
+read file
+echo "Enter a regex:"
+read regex
+grep -c $regex $file
+
+echo "Enter a regex"
+read regex2
+grep -o "303.*$" $file
+
+echo "Enter email"
+read email
+grep -o "^.*@geocities.com$" $file >> email_results.txt
